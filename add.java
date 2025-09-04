532ba03f1e519dc3f1e5c2f222667e33a3a7e3e7
rohansirohi7 <sirohirohan771@gmail.com>
2025-09-04 11:10:43 +0530

    java progarm

---
+++ add.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class add {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("enter the first number");
+        int num1= sc.nextInt();
+        System.out.println("enter the second number");
+        int num2 = sc.nextInt();
+        int sum =num1+num2;
+        System.out.println("sum of two number"+sum);
+    }
+}
+
+
